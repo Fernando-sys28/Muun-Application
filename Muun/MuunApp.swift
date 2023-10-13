4d37eeae70aabc05542c0e697ecdebe5014d3c52
Fernando-sys28 <fercho_cas09@hotmail.com>
2023-10-12 20:10:25 -0600

    Initial Commit

---
+++ Muun/MuunApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuunApp.swift
+//  Muun
+//
+//  Created by Fernando Rivera Castillo on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MuunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
